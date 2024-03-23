6dae04ef81377423243442b11f25e92d4583f9e7
Kevin Bonds <kevin.bonds@gmail.com>
2024-03-23 14:39:19 -0500

    updated the table

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -14,8 +14,8 @@
     <div class="container">
         <h1 class="text-center">Products</h1>
         <div class="row">
-            <div class="col-md-4"></div>
-            <div class="col-md-4">
+            <div class="col-md-2"></div>
+            <div class="col-md-8">
                 <table class="table table-primary table-striped">
                     <thead>
                         <tr>
@@ -39,7 +39,7 @@
                     </tbody>
                 </table>
             </div>
-            <div class="col-md-4"></div>
+            <div class="col-md-2"></div>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
